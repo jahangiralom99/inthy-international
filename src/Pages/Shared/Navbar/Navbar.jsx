0680b3f0f3453c46a56f0d2a0996132c481561a8
jahangir <jahangiralomtuf99@gmail.com>
2024-08-14 15:44:56 +0600

    update navbar

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -1,10 +1,93 @@
-
+import { NavLink } from "react-router-dom";
+import logo from "../../../assets/Logo-new.png";
+import PhoneLogo from "../../../assets/phone.png";
+import { IoMenu } from "react-icons/io5";
+import { useState } from "react";
 const Navbar = () => {
-    return (
+  const [open, setOpen] = useState(false);
+
+  const navLinks = (
+    <>
+      <NavLink
+        to="/"
+        className={({ isActive, isPending }) =>
+          isPending ? "pending" : isActive ? "text-[#f97c00] " : ""
+        }
+      >
+        Home
+      </NavLink>
+      <hr className="lg:hidden" />
+      <NavLink
+        to="/product"
+        className={({ isActive, isPending }) =>
+          isPending
+            ? "pending"
+            : isActive
+            ? "text-[#f97c00]"
+            : " hover:text-[#f97c00]"
+        }
+      >
+        Product
+      </NavLink>
+      <hr className="lg:hidden" />
+      <NavLink
+        to="/about-us"
+        className={({ isActive, isPending }) =>
+          isPending ? "pending" : isActive ? "text-[#f97c00]" : ""
+        }
+      >
+        About Us
+      </NavLink>
+      <hr className="lg:hidden" />
+      <NavLink
+        to="/contact"
+        className={({ isActive, isPending }) =>
+          isPending ? "pending" : isActive ? "text-[#f97c00]" : ""
+        }
+      >
+        Contact Us
+      </NavLink>
+      <hr className="lg:hidden" />
+    </>
+  );
+
+  return (
+    <nav className="border">
+      <div className="flex justify-between items-center  p-2 max-w-screen-xl mx-auto px-4">
         <div>
-            navbar
+          <img className="w-36" src={logo} alt="logo" />
         </div>
-    );
+        <div className="lg:flex gap-6 uppercase font-semibold hidden">
+          {navLinks}
+        </div>
+        <div className="lg:flex items-center gap-2 font-semibold  hidden">
+          <img className="w-8" src={PhoneLogo} alt="phone-icon" />
+          <p>01925921227/01675957881</p>
+        </div>
+        {/* side Bar for Phone  */}
+        <div className="lg:hidden">
+          <div onClick={() => setOpen(!open)}>
+            <IoMenu className="text-5xl bg-[#549708] rounded-full text-white p-2" />
+          </div>
+          {open && (
+            <div
+              onClick={() => setOpen(false)}
+              className="absolute top-0 left-0 h-screen w-full bg-black bg-opacity-60"
+            ></div>
+          )}
+          <div
+            className={`absolute bg-[#f5f5f5] flex flex-col gap-5 border p-5 ${
+              open
+                ? "w-[60%] md:w-[40%] h-full top-0 right-0 transition duration-500 delay-200"
+                : "-left-56 top-0"
+            }`}
+          >
+            {navLinks}
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
